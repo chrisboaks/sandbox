39202609a8538df4288a1ea24f96c478112dfb78
Christopher Boaks <chris.boaks@gmail.com>
2015-03-30 22:47:32 -0400

    Basic objects

--- myparser.py
+++ myparser.py
@@ -1,7 +1,7 @@
 import collections
 import re
 
-Token = collections.namedtuple('Token', ['typ', 'value', 'line', 'column'])
+Token = collections.namedtuple('Token', ['identifier', 'value', 'line', 'column'])
 
 
 def tokenize(prison):
@@ -26,35 +26,85 @@ def tokenize(prison):
             line_start = pos
             line += 1
         elif identifier != 'WHITESPACE':
-            val = matched.group(identifier)
+            value = matched.group(identifier)
             if identifier == 'STRING':
-                val = val.strip('"')
-            yield Token(identifier, val, line, matched.start() - line_start)
+                value = value.strip('"')
+            yield Token(identifier, value, line, matched.start() - line_start)
         pos = matched.end()
         matched = get_token(prison, pos)
     if pos != len(prison):
         raise RuntimeError('Unexpected character %r on line %d' % (prison[pos], line))
 
-inFile = r"mini.prison"
-with open(inFile, "r") as prison:
+infile = r"mini.prison"
+with open(infile, "r") as prison:
     tokens = tokenize(prison.read())
     for token in tokens:
         print(token)
 
 
-# inFile = r"orig.prison"
-# outFile = r"parsed.prison"
-# with open(inFile, "r") as oldPrisonFile, open(outFile, "w") as newPrisonFile:
-#     scanner = re.Scanner([
-#         (r"\s+", None),
-#         (r"BEGIN", lambda scanner, token:(TOKEN_BEGIN, token)),
-#         (r"END", lambda scanner, token:(TOKEN_END, token)),
-#         (r'".*"', lambda scanner, token:(TOKEN_CONTENT, token)),
-#         (r"[^\s]*", lambda scanner, token:(TOKEN_CONTENT, token)),
-#     ])
+class BaseObj:
+    all = []
+    obj_attrs = []
 
-#     tokens, remainder = scanner.scan(oldPrisonFile.read())
-#     parsed_prison = parse_tokens(tokens)[0]
+    @classmethod
+    def filter(cls, **kwargs):
+        possibilities = list(cls.all)
+        for k, v in kwargs.iteritems():
+            if k not in cls.obj_attrs:
+                raise NameError('Class %s has no attribute %s' % (cls, k))
+            possibilities = filter(lambda x: getattr(x, k) == v, possibilities)
+        return possibilities
 
-#     security_hearings(parsed_prison)
-#     newPrisonFile.write("\n".join(generate_prison_format(parsed_prison)))
+
+class Attribute(BaseObj):
+    obj_attrs = ['name', 'value', 'token', 'owner']
+
+    def __repr__(self):
+        return '<Attribute %s:%s>' % (self.name, self.value)
+
+    def __init__(self, name, value, token, owner=None):
+        self.name = name
+        self.value = value
+        self.token = token
+        self.owner = owner
+        Attribute.all.append(self)
+
+
+class PrisonObj(BaseObj):
+    def __init__(self, name):
+        self.name = name
+        self.attributes = []
+        PrisonObj.all.append(self)
+
+    def __repr__(self):
+        return 'PrisonObj %s' % self.name
+
+    def add_attribute(self, attribute):
+        attribute.owner = self
+        self.attributes.append(attribute)
+
+
+a = Attribute('a', 3, 'tokena')
+b = Attribute('b', 4, 'tokenb')
+
+
+# def parse_tokens(tokens):
+#     content = OrderedDict()
+#     index = 0
+#     if type(tokens) is list:
+#         tokens = enumerate(tokens)
+#     for index, token in tokens:
+#         if token[0] == TOKEN_END:
+#             break
+#         elif token[0] == TOKEN_BEGIN:
+#             secondIndex, secondToken = next(tokens)
+#             if secondToken[0] != TOKEN_CONTENT:
+#                 raise Exception("Expected a CONTENT token after a BEGIN token")
+#             parsed_tokens, sub_contentLength = parse_tokens(tokens)
+#             content.setdefault(secondToken[1].strip('"'), [])
+#             content[secondToken[1].strip('"')].append(parsed_tokens)
+#         else:
+#             secondIndex, secondToken = next(tokens)
+#             content.setdefault(token[1].strip('"'), [])
+#             content[token[1].strip('"')].append(secondToken[1])
+#     return content, index
